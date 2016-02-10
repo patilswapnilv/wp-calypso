065378b552a2c8fddb75501e51e3b87815a58d22
Bernhard Reiter <ockham@raz.or.at>
2016-02-10 23:13:25 +0100

    ES6ify

--- client/my-sites/themes/controller.js
+++ client/my-sites/themes/controller.js
@@ -1,79 +1,74 @@
 /**
  * External Dependencies
  */
-var ReactDom = require( 'react-dom' ),
-	React = require( 'react' ),
-	ReduxProvider = require( 'react-redux' ).Provider;
+import ReactDom from 'react-dom';
+import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 
 /**
  * Internal Dependencies
  */
-var SingleSiteComponent = require( 'my-sites/themes/single-site' ),
-	MultiSiteComponent = require( 'my-sites/themes/multi-site' ),
-	LoggedOutComponent = require( 'my-sites/themes/logged-out' ),
-	analytics = require( 'analytics' ),
-	i18n = require( 'lib/mixins/i18n' ),
-	trackScrollPage = require( 'lib/track-scroll-page' ),
-	getCurrentUser = require( 'state/current-user/selectors' ).getCurrentUser,
-	buildTitle = require( 'lib/screen-title/utils' ),
-	getAnalyticsData = require( './helpers' ).getAnalyticsData,
-	ClientSideEffects = require( './client-side-effects' );
+import SingleSiteComponent from 'my-sites/themes/single-site';
+import MultiSiteComponent from 'my-sites/themes/multi-site';
+import LoggedOutComponent from 'my-sites/themes/logged-out';
+import analytics from 'analytics';
+import i18n from 'lib/mixins/i18n';
+import trackScrollPage from 'lib/track-scroll-page';
+import { getCurrentUser } from 'state/current-user/selectors';
+import buildTitle from 'lib/screen-title/utils';
+import { getAnalyticsData } from './helpers';
+import ClientSideEffects from './client-side-effects';
 
-var controller = {
+export function themes( context ) {
+	const { tier, site_id } = context.params;
+	const user = getCurrentUser( context.store.getState() );
+	const title = buildTitle(
+		i18n.translate( 'Themes', { textOnly: true } ),
+		{ siteID: site_id } );
+	const Head = user
+		? require( 'layout/head' )
+		: require( 'my-sites/themes/head' );
 
-	themes: function( context ) {
-		const { tier, site_id } = context.params;
-		const user = getCurrentUser( context.store.getState() );
-		const title = buildTitle(
-			i18n.translate( 'Themes', { textOnly: true } ),
-			{ siteID: site_id } );
-		const Head = user
-			? require( 'layout/head' )
-			: require( 'my-sites/themes/head' );
+	let ThemesComponent;
 
-		let ThemesComponent;
-
-		if ( user ) {
-			ThemesComponent = site_id ? SingleSiteComponent : MultiSiteComponent;
-		} else {
-			ThemesComponent = LoggedOutComponent;
-		}
-
-		const { basePath, analyticsPageTitle } = getAnalyticsData(
-			context.path,
-			tier,
-			site_id
-		);
-
-		const runClientAnalytics = function() {
-			analytics.pageView.record( basePath, analyticsPageTitle );
-		};
-
-		const boundTrackScrollPage = function() {
-			trackScrollPage(
-				basePath,
-				analyticsPageTitle,
-				'Themes'
-			);
-		};
-
-		ReactDom.render(
-			<ReduxProvider store={ context.store }>
-				<Head title={ title } tier={ tier || 'all' }>
-					<ThemesComponent
-						key={ site_id }
-						siteId={ site_id }
-						tier={ tier }
-						search={ context.query.s }
-						trackScrollPage={ boundTrackScrollPage } />
-					<ClientSideEffects>
-						{ runClientAnalytics }
-					</ClientSideEffects>
-				</Head>
-			</ReduxProvider>,
-			document.getElementById( 'primary' )
-		);
+	if ( user ) {
+		ThemesComponent = site_id ? SingleSiteComponent : MultiSiteComponent;
+	} else {
+		ThemesComponent = LoggedOutComponent;
 	}
-};
 
-module.exports = controller;
+	const { basePath, analyticsPageTitle } = getAnalyticsData(
+		context.path,
+		tier,
+		site_id
+	);
+
+	const runClientAnalytics = function() {
+		analytics.pageView.record( basePath, analyticsPageTitle );
+	};
+
+	const boundTrackScrollPage = function() {
+		trackScrollPage(
+			basePath,
+			analyticsPageTitle,
+			'Themes'
+		);
+	};
+
+	ReactDom.render(
+		<ReduxProvider store={ context.store }>
+			<Head title={ title } tier={ tier || 'all' }>
+				<ThemesComponent
+					key={ site_id }
+					siteId={ site_id }
+					tier={ tier }
+					search={ context.query.s }
+					trackScrollPage={ boundTrackScrollPage } />
+				<ClientSideEffects>
+					{ runClientAnalytics }
+				</ClientSideEffects>
+			</Head>
+		</ReduxProvider>,
+		document.getElementById( 'primary' )
+	);
+}
